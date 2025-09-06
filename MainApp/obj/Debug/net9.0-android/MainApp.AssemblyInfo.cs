6b419af021fa5b680e5212e72ba99df60e548297
suhailfallel <116296538+suhailfallel@users.noreply.github.com>
2025-09-06 08:28:28 +0400

    Minimal Change in Routing

--- MainApp/obj/Debug/net9.0-android/MainApp.AssemblyInfo.cs
+++ MainApp/obj/Debug/net9.0-android/MainApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d0a8a5596c22d262938ebcad775842fc0e73af58")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
